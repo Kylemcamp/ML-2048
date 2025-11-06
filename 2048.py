75c225f62f635d3a1297eb196ed94ce77cc9e5ac
Kyle Campbell <kmcampb1@ualberta.ca>
2025-11-06 16:30:31 -0700

    initial 2048 class

---
+++ 2048.py
@@ -0,0 +1,42 @@
+import math
+import random
+
+class game2048:
+    def __init__(self):
+        self.board_size = 4
+        self.score = 0
+        self.board = [[0] * 4 for _ in range(4)]
+        self.game_over = False
+
+        # likelihood of spawning each num
+        self.two_percentage = 0.9
+        self.four_percentage = 0.1
+
+        # game starts with 2 tiles on board
+        self.add_tile()
+        self.add_tile()
+
+    def display_board(self):
+        for i in range(self.board_size):
+            if i % self.board_size == 0 and i != 0:
+                print()
+            print(self.board[i])
+
+    def add_tile(self):
+        if random.random() < self.two_percentage:
+            tile_val = 2
+        else:
+            tile_val = 4
+
+        # Find an empty spot on the board
+        empty_spots = [(row, col) for row in range(self.board_size) for col in range(self.board_size) if self.board[row][col] == 0]
+        if empty_spots:
+            row, col = random.choice(empty_spots)
+            self.board[row][col] = tile_val
+
+def main():
+    game = game2048()
+    game.display_board()
+
+if __name__ == "__main__":
+    main()
